ff529f32fd6d44d06e1547f291b42f8cb5a1ff5c
sendafr <sendawulafredrick@gmail.com>
2026-03-06 05:27:40 -0500

    'Home'

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { profileAPI } from './api2';
 import { useAuthentication } from '../Auth';
 import styles from './Home.module.css';
+import VideoUload from './Components/VideoUload.jsx';
 
 export default function Home() {
   const { logout }              = useAuthentication();
@@ -121,6 +122,8 @@ export default function Home() {
 
   // ─── Render ───────────────────────────────────────────────────────────────
   return (
+    <>
+    <VideoUload/>
     <div className={styles.page}>
       <div className={styles.container}>
 
@@ -324,5 +327,6 @@ export default function Home() {
 
       </div>
     </div>
+    </>
   );
 }
