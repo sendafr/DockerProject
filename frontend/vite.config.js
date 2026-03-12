5c85737fa4116af18cc44985e2cc31c339e55d9e
sendafr <sendawulafredrick@gmail.com>
2026-03-12 06:26:01 -0400

    gd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     proxy: {
       // All /api requests are forwarded to Django backend
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'src_authentic:8000',
         changeOrigin: true,
         secure: false,
       },
