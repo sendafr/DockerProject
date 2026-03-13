6524e43da5191c96c65aa6d0e679f70c8c974a70
sendafr <sendawulafredrick@gmail.com>
2026-03-13 02:52:09 -0400

    url

--- src_authentic/authentic/urls.py
+++ src_authentic/authentic/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api.views import*
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
 #from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 
@@ -47,5 +53,9 @@ urlpatterns = [
     #path("", include('api.urls')),
 
 ]
+# only when DEBUG is True – `runserver` will serve the files
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 
     
